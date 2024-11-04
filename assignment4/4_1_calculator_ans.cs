9d076ad9291beb41b0f421ba7df02ad4bac9cf61
dong0625 <dong0625@korea.ac.kr>
2024-11-04 18:32:04 +0900

    Update 4_1_calculator_ans.cs

--- assignment4/4_1_calculator_ans.cs
+++ assignment4/4_1_calculator_ans.cs
@@ -50,7 +50,22 @@ namespace calculator
     public class Calculator
     {
         // ---------- TODO ----------
-        
+        public double Calculate(double l, string op, double r)
+        {
+            if (op == "+")
+                return l + r;
+            else if (op == "-")
+                return l - r;
+            else if (op == "*")
+                return l * r;
+            else if (op == "/")
+                if (r == 0)
+                    throw new DivideByZeroException("Division by zero is not allowed");
+                else
+                    return l / r;
+            else
+                throw new InvalidOperationException("Invalid operator");
+        }
         // --------------------
     }
 }
